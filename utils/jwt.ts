89fbeda871dc5275a66e027e9d2b8284f0dca848
AssanRabari <assandesai@gmail.com>
2024-07-08 09:58:48 +0530

    login user created jwt file

---
+++ utils/jwt.ts
@@ -0,0 +1,12 @@
+require("dotenv").config();
+import { Request, Response, NextFunction } from "express";
+import { IUser } from "../models/user.model";
+import { redis } from "./redis";
+interface ITokenOptions {
+  expires: Date;
+  maxAge: number;
+  httpOnly: boolean;
+  sameSite: "lax" | "strict" | "none" | undefined;
+  secure?:boolean
+}
+const const sendToken =(user:IUser,statusCode:number)
