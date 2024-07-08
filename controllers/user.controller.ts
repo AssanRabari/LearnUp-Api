a6582d6cb28a3fd91c78b123bb27e95d187d8909
AssanRabari <assandesai@gmail.com>
2024-07-08 09:52:24 +0530

    Login user initial changes

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -116,9 +116,38 @@ export const activateUser = catchAsyncError(
         password,
       });
 
-      res.status(201).json({success:true})
+      res.status(201).json({ success: true });
     } catch (error: any) {
       return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
+//Login user
+
+interface ILoginRequest {
+  email: string;
+  password: string;
+}
+export const loginUser = catchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { email, password } = req.body as ILoginRequest;
+
+      if (!email || !password) {
+        return next(new ErrorHandler("please enter email and password", 400));
+      }
+
+      const user = await userModel.findOne({ email }).select("+password");
+      if (!user) {
+        return next(new ErrorHandler("Invalid email and password", 400));
+      }
+
+      const isPasswordMatch = await user.comparePassword(password);
+      if (!isPasswordMatch) {
+        return next(new ErrorHandler("Invalid email and password", 400));
+      }
+    } catch (error: any) {
+      next(new ErrorHandler(error.message, 400));
     }
   }
 );
