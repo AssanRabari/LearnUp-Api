33dc6f3f013761d861d4c234d0e4f45f19a746b7
AssanRabari <assandesai@gmail.com>
2024-07-16 09:36:35 +0530

    course controller inital changes

---
+++ controllers/course.controller.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from "express";
+import { catchAsyncError } from "../middleware/catchAsyncError";
+import ErrorHandler from "../utils/ErrorHandler";
+import cloudinary from "cloudinary";
+
+//upload course
+export const uploadCourse = catchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const data = req.body;
+      const thumbnail = data.thumbnail;
+      if (thumbnail) {
+        const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
+          folder: "courses",
+        });
+        data.thumbnail = {
+          public_id: myCloud.public_id,
+          url: myCloud.secure_url,
+        };
+      }
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
