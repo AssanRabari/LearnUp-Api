b280e35010637a4b843f4e101cd69089de2ad4ae
AssanRabari <assandesai@gmail.com>
2024-07-27 09:31:06 +0530

    Added node-cron for delete notification

--- controllers/notification.controller.ts
+++ controllers/notification.controller.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { catchAsyncError } from "../middleware/catchAsyncError";
 import ErrorHandler from "../utils/ErrorHandler";
 import notificationModel from "../models/notification.model";
+import cron from "node-cron";
 
 //get all notifications -- only admin have access
 export const getNotifications = catchAsyncError(
@@ -45,3 +46,13 @@ export const updateNotificationStatus = catchAsyncError(
     }
   }
 );
+
+//delete notification after while using node-cron
+cron.schedule("0 0 0 * * *", async () => {
+  const thirtyDaysAgo = new Date(Date.now() - 30 * 24 * 60 * 100);
+
+  await notificationModel.deleteMany({
+    status: "read",
+    createdAt: { $lt: thirtyDaysAgo },
+  });
+});
